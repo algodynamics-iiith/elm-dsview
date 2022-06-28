a984a4f628f12455cd736ef6fe979a7e9950d885
mridulssgss <jhamrityunjay1999@gmail.com>
2022-06-28 12:01:50 +0530

    refractor: split the attributes into render and layout files

--- src/Render/Attributes.elm
+++ src/Render/Attributes.elm
@@ -1,7 +1,6 @@
-module Attributes exposing (Direction(..), DrawConfig, LayoutConfig, direction, elemDistX, elemDistY, elemDrawer, fill, height, heightDict, label, marginX, marginY, onClick, shape, strokeColor, strokeDashArray, strokeWidth, style, svgDrawNode, title, width, widthDict, wrapVal, xLabel, xLabelPos)
+module Render.Attributes exposing (..)
 
 import Color exposing (Color)
-import Dict exposing (Dict)
 import Graph exposing (Node)
 import Render as R
 import Render.StandardDrawers as RSD
@@ -9,77 +8,6 @@ import Render.StandardDrawers.Attributes exposing (Attribute)
 import Render.StandardDrawers.Types exposing (..)
 import Render.Types exposing (..)
 import TypedSvg.Core exposing (Svg)
-
-
-type Direction
-    = TB
-    | BT
-    | LR
-    | RL
-
-
-type alias LayoutConfig =
-    { widthDict : Dict Int Float
-    , heightDict : Dict Int Float
-    , width : Float
-    , height : Float
-    , marginX : Float
-    , marginY : Float
-    , elemDistX : Float
-    , elemDistY : Float
-    , direction : Direction
-    , wrapVal : Maybe Int
-    }
-
-
-widthDict : Dict Int Float -> Attribute { b | widthDict : Dict Int Float }
-widthDict wdict =
-    \rec -> { rec | widthDict = wdict }
-
-
-heightDict : Dict Int Float -> Attribute { b | heightDict : Dict Int Float }
-heightDict hdict =
-    \rec -> { rec | heightDict = hdict }
-
-
-width : Float -> Attribute { b | width : Float }
-width w =
-    \rec -> { rec | width = w }
-
-
-height : Float -> Attribute { b | height : Float }
-height h =
-    \rec -> { rec | height = h }
-
-
-marginX : Float -> Attribute { b | marginX : Float }
-marginX mX =
-    \rec -> { rec | marginX = mX }
-
-
-marginY : Float -> Attribute { b | marginY : Float }
-marginY mY =
-    \rec -> { rec | marginY = mY }
-
-
-elemDistX : Float -> Attribute { b | elemDistX : Float }
-elemDistX dx =
-    \rec -> { rec | elemDistX = dx }
-
-
-elemDistY : Float -> Attribute { b | elemDistY : Float }
-elemDistY dy =
-    \rec -> { rec | elemDistY = dy }
-
-
-direction : Direction -> Attribute { b | direction : Direction }
-direction dir =
-    \rec -> { rec | direction = dir }
-
-
-wrapVal : Maybe Int -> Attribute { b | wrapVal : Maybe Int }
-wrapVal wv =
-    \rec -> { rec | wrapVal = wv }
 
 
 type alias DrawConfig n e msg =
