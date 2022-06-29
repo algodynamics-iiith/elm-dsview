4ee1ddfed76cae3d14021514d1160b3c346519e4
shivankar <shivankarpilligundla@gmail.com>
2022-06-29 11:50:03 +0530

    updated example

--- examples/Main.elm
+++ examples/Main.elm
@@ -15,20 +15,14 @@ main : Html msg
 main =
     AV.draw
         [ direction LR
-        --, width 10
-        {- , widthDict <| Dict.fromList [ ( 0, 50 ), ( 1, 60 ), ( 2, 70 ), ( 3, 80 ), ( 4, 90 ), ( 5, 100 ) ]
-        , heightDict <| Dict.fromList [ ( 0, 40 ), ( 1, 50 ), ( 2, 60 ), ( 3, 70 ), ( 4, 80 ), ( 5, 90 ) ] -}
         ]
         [ elemDrawer
             (svgDrawNode
                 [ A.label (\n -> String.fromInt n.label)
                 , A.xLabel (\n -> String.fromInt n.id)
                 , A.xLabelPos (\_ _ _ -> ( 0, 65 ))
-                {- , A.shape A.Box
-                , A.fill (\_ -> Color.rgb 255 255 255)
+                , A.shape A.Box
                 , A.strokeColor (\_ -> Color.rgb 0 0 0)
-                , A.strokeDashArray (\_ -> "hi")
-                , A.xLabelPos (\_ _ _-> (0, 40)) -}
                 ]
             )
         ]
